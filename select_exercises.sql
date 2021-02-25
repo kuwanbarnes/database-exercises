02c9ab2b6260101269fa7d90a176ea75c6606473
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-25 14:28:29 -0600

    update

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,8 +5,9 @@ SELECT release_date AS 'DATE' FROM albums WHERE name = 'Sgt. Pepper''s Lonely He
 # The genre for Nevermind
 SELECT  genre AS 'GENRE TYPE' FROM albums WHERE name ='Nevermind';
 # Which albums were released in the 1990s
-SELECT name AS '1990s hits' FROM albums WHERE release_date >=1990;
-# Which albums had less than 20 million certified sales
-SELECT name AS '20MIL_SOLD' FROM albums WHERE sales >=20.0;
+SELECT  name FROM albums WHERE  release_date >=1990 && release_date < 2000;
+        # Which albums had less than 20 million certified sales
+SELECT name AS '20MIL_SOLD' FROM albums WHERE sales <=20.0;
 # All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 SELECT name AS 'ROCK MUSIC' FROM  albums WHERE genre = 'Rock';
+#SELECT name AS 'ROCK MUSIC' FROM  albums WHERE genre LIKE = '%rock%';
---
+++ update_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+# All albums in your table.
+SELECT * FROM albums ;
+UPDATE albums
+SET sales= sales *10;
+
+#All albums released before 198
+SELECT name AS 'BEFORE 1980s hits' FROM albums WHERE release_date <=1980;
+UPDATE albums
+SET  release_date= release_date-100
+WHERE release_date< 1980;
+# All albums by Michael Jackson
+SELECT  name AS 'MJ' FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
