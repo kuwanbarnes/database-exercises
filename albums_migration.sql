7c5face81764997db0e79159ba436cec8bed3188
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-25 11:25:32 -0600

    seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,12 @@
 USE codeup_test_db;
 
-DROP table if exists albums;
+DROP table IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
-    name  VARCHAR(100),
-    release_date INT(4) UNSIGNED,
-    sales FLOAT ,
-    genre TEXT ,
+    artist VARCHAR(50) NOT NULL,
+    name  VARCHAR(100) NOT NULL,
+    release_date INT(4) NOT NULL,
+    sales DECIMAL (6,3) NOT NULL,
+    genre VARCHAR (100),
     PRIMARY KEY (id)
 );
