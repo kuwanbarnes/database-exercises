e673568b9aa147531cbfecc905aa59e69f03f9e0
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-26 15:32:16 -0600

    functions final

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -10,7 +10,13 @@ SELECT * FROM employees WHERE  first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY
 
 #Update your queries for employees with 'E' in their last name to sort the results by their employee number.
 SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no ;
-SELECT CONCAT_WS(' ',first_name , last_name) FROM employees WHERE last_name LIKE 'E%' OR  last_name LIKE '%E'ORDER BY emp_no;
+SELECT CONCAT_WS(' ',first_name , last_name)
+FROM employees
+WHERE last_name LIKE 'E%' OR  last_name LIKE '%E'
+ORDER BY emp_no ;
+
+SELECT emp_no,birth_date, CONCAT_WS(' ',first_name , last_name)
+FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC ;
 
 
 
@@ -20,8 +26,15 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
 #first result is the oldest employee who was hired lastx
 
 SELECT CONCAT_WS(' ',first_name,last_name,'- Days worked: ',
-    datediff(now(),hire_date)) FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25' ORDER BY birth_date,hire_date DESC ;
+    datediff(now(),hire_date))
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date,hire_date DESC ;
 
-
+SELECT * ,datediff(curdate(),hire_date)
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date,hire_date DESC ;
 SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%' ;
 
